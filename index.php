3fc5e36c8df38160f2e65dbe0390b7eb64f7c245
regisrob <regisrobineau@gmail.com>
2015-04-09 10:45:12 +0200

    Remove meta X-UA-Compatible

--- index.php
+++ index.php
@@ -6,7 +6,6 @@ require_once('common.php');
 <html lang="fr">
 <head>
   <meta charset="utf-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <title>IIIF Manifests Repository - Biblissima</title>
   
