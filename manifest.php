dc95ac9dce8989160b84b8486faf7a8082fd63e4
regisrob <regisrobineau@gmail.com>
2015-11-25 17:03:57 +0100

    * Prevent php Notice "Array to string conversion" * default to php >= 5.4

--- manifest.php
+++ manifest.php
@@ -39,15 +39,16 @@ if ( !IsManifestInDb( $collections, $mfId ) ) {
   foreach ($cursor as $doc) {
     
     // For PHP >= 5.4
-    //echo json_encode($doc, JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE);
+    $mf = json_encode($doc, JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE);
+    echo $mf;
     
     // For Mongodb 1.4.4 (current version on Debian 6)
-    unset($doc['_id']); // remove _id field from result, since projection operator does not seem to work w/ Mongodb 1.4.4
+    //unset($doc['_id']); // remove _id field from result, since projection operator does not seem to work w/ Mongodb 1.4.4
     
     // For PHP <= 5.4 (json_encode constants not supported)
-    $doc = str_replace('\\/', '/', json_encode($doc));
+    //$doc = str_replace('\\/', '/', json_encode($doc));
     //$doc = mb_convert_encoding($doc, 'UTF-8');
-    echo $doc;
+    //echo $doc;
   }
 }
 
